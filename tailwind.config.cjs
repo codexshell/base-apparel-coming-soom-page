e41ecab05120768b36635ef0f9b2e6e9d1e81971
silvanos <ericoderito@gmail.com>
2022-08-19 20:00:14 +0300

    extends tailwind css classes using provided styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				'p-desaturated-red': 'hsl(0, 36%, 70%)',
+				'p-soft-red': 'hsl(0, 93%, 68%)',
+				'n-dark-grayish-red': 'hsl(0, 6%, 24%)'
+			},
+			fontFamily: {
+				'josefin-sans': ['Josefin Sans', 'sans-serif']
+			}
+		}
 	},
 	plugins: []
 };
