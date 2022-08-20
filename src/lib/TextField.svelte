6ec1d7c63b2ef56c4c95db5a5286f0635746bdc3
silvanos <ericoderito@gmail.com>
2022-08-20 08:50:58 +0300

    add htm content

---
+++ src/lib/TextField.svelte
@@ -0,0 +1,8 @@
+<script></script>
+
+<form>
+  <input type="email" placeholder="Email Address" required>
+  <button type="submit">Submit</button>
+</form>
+
+<style></style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+	import TextField from '$lib/TextField.svelte';
 </script>
 
 <svelte:head>
@@ -6,6 +7,28 @@
 	<meta name="description" content="coming soon page demo" />
 </svelte:head>
 
-<section></section>
+<section>
+	<div class="section__content">
+		<!-- * section logo -->
+		<div class="logo">
+			<img src="/logo.svg" alt="" />
+		</div>
+	</div>
+
+	<h1><span>We're</span> coming sooon</h1>
+
+	<p>
+		Hello fellow shoppers! We're currently building our new fashion store. Add your email below to
+		stay up-to-date with announcements and our launch deals.
+	</p>
+
+	<TextField />
+	<div class="hero">
+		<picture>
+			<source media="(max-width: 40rem)" srcset="/hero-mobile.jpg" />
+			<img src="/hero-desktop.jpg" alt="" />
+		</picture>
+	</div>
+</section>
 
 <style></style>
